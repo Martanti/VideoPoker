ad3cb8b82769219df8ee881f4d3125194709af94
Martynas Antipenkovas <martynas.antipenkovas@hotmail.lt>
2019-05-31 18:03:07 +0300

    Starting commit
    Created card struct
    Created card population actions
    Started woring on UI and main game cycle

---
+++ VideoPoker_Visma/GameManagment/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManagment
+{
+
+    /*
+     * enums are chosen in order to avoid typos when checking, creatig card symbols and their ranks
+     */
+
+    public enum CardSymbol {Clubs, Diamonds, Hearts, Spades }
+    public enum CardRank { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+
+    struct Card
+    {
+        public CardSymbol cardSymbol;
+        public CardRank cardRank;
+
+    }
+}
---
+++ VideoPoker_Visma/GameManagment/CardManagment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManagment
+{
+    class CardManagment
+    {
+        private List<Card> availableCards;
+
+        public CardManagment()
+        {
+            availableCards = new List<Card>();
+        }
+
+
+        public void PopulateDeck()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                CardSymbol symbol = (CardSymbol)i;
+
+                for (int j = 0; j < 13; j++)
+                {
+                    CardRank rank = (CardRank)j;
+
+                    availableCards.Add(new Card {
+                        cardRank = rank,
+                        cardSymbol = symbol
+                    });
+                }
+            }
+        }
+
+
+        public Card TakeOutRandomCard()
+        {
+            Random random = new Random();
+
+            int randomCardIndex =  random.Next(availableCards.Count);
+
+            Card cardToGiveOut = availableCards[randomCardIndex];
+
+            availableCards.Remove(cardToGiveOut);
+
+            return cardToGiveOut;
+        }
+
+        public bool CanDraw(int neededAmountToDraw)
+        {
+            if (neededAmountToDraw<=availableCards.Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
